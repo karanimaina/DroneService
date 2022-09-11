e3ab9bd82c95cce23dd396e3ccbc61f30d85ef8d
felixmaina <karani.maina2010@gmail.com>
2022-09-11 12:51:28 +0300

    first commit

---
+++ src/test/java/com/example/drone/DroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
