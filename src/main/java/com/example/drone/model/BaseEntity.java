ebaf24dbfa0c30ab2c02c358ec09612e0fa932c0
felixmaina <karani.maina2010@gmail.com>
2022-09-11 18:30:55 +0300

    create a soft delete class

--- src/main/java/com/example/drone/model/BaseEntity.java
+++ src/main/java/com/example/drone/model/BaseEntity.java
@@ -1,14 +1,39 @@
 package com.example.drone.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
-
 public class BaseEntity {
+   @Id
+   @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "created+on",updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Africa/Nairobi")
+    public Date created_on;
+    @Column(name = "softDelete")
+    @JsonIgnore
+    public boolean softDelete;
+    @PrePersist
+    public void addData(){
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        ZoneId zoneId  = ZoneId.of("Africa/Nairobi");
+        ZonedDateTime nairobi =  zonedDateTime.withZoneSameInstant(zoneId);
+        this.created_on = Date.from(nairobi.toInstant());
+        this.softDelete = false;
+    }
+
+
+
 }
