a8e113598db3cc589789ace4859a6f6474c8db57
felixmaina <karani.maina2010@gmail.com>
2022-09-11 14:05:09 +0300

    add spring validationm dependency

--- src/main/java/com/example/drone/model/drone.java
+++ src/main/java/com/example/drone/model/drone.java
@@ -1,16 +1,30 @@
 package com.example.drone.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
-
+@Table
+@AllArgsConstructor
+@Getter
+@Setter
+=
+@NoArgsConstructor
 public class drone {
-
+    @Size(min = 3,max = 100)
     private  String serialNo;
+    @Enumerated(EnumType.STRING)
     private String model;
+    @DecimalMax(value = "500")
     private String weightLimit;
+    @Max(value = 100)
     private int batteryPercentage;
+    @Enumerated(EnumType.STRING)
     private DroneState state;
 
 }
