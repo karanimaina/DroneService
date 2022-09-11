106417f4319959f93244cf856697b66f280236e3
felixmaina <karani.maina2010@gmail.com>
2022-09-11 13:32:16 +0300

    create a drone class

---
+++ src/main/java/com/example/drone/model/Delivery.java
@@ -0,0 +1,4 @@
+package com.example.drone.model;
+
+public class Delivery {
+}
---
+++ src/main/java/com/example/drone/model/drone.java
@@ -0,0 +1,10 @@
+package com.example.drone.model;
+
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Table;
+
+
+public class drone {
+
+}
