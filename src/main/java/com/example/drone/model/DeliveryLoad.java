c3f1d4619c748ce29cd4e3038c8c7a181f940262
felixmaina <karani.maina2010@gmail.com>
2022-09-11 18:02:19 +0300

    add delivery Load class

--- src/main/java/com/example/drone/model/DeliveryLoad.java
+++ src/main/java/com/example/drone/model/DeliveryLoad.java
@@ -1,8 +1,13 @@
 package com.example.drone.model;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
 
 public class DeliveryLoad {
     @ManyToOne
-    private Drone
+    private Drone drone;
+    @Enumerated (EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+    private double loadWeight = 0;
 }
