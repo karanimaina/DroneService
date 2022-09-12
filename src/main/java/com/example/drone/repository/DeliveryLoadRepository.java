3ce1dc82ed7c806cd1dde63b14e325fb21eb7627
felixmaina <karani.maina2010@gmail.com>
2022-09-12 08:11:31 +0300

    add a delivery Load interface that extends ftom jpa

---
+++ src/main/java/com/example/drone/repository/DeliveryLoadRepository.java
@@ -0,0 +1,8 @@
+package com.example.drone.repository;
+
+import com.example.drone.model.DeliveryLoad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryLoadRepository extends JpaRepository<DeliveryLoad,Long> {
+
+}
