d90f0f915bcd6842f52a7d8c5ec1f645fc2f0bfb
felixmaina <karani.maina2010@gmail.com>
2022-09-12 13:31:44 +0300

    check if drone exists

--- src/main/java/com/example/drone/service/DroneServiceImpl.java
+++ src/main/java/com/example/drone/service/DroneServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.drone.service;
 
+import com.example.drone.exceptions.ItemNotFoundException;
 import com.example.drone.model.Delivery;
 import com.example.drone.model.DeliveryLoad;
 import com.example.drone.model.Drone;
+import com.example.drone.model.Medicine;
 import com.example.drone.repository.DeliveryRepository;
 import com.example.drone.repository.DroneRepository;
 import com.example.drone.repository.MedicineRepository;
@@ -11,7 +13,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class DroneServiceImpl implements  DroneService{
@@ -21,15 +26,24 @@ public class DroneServiceImpl implements  DroneService{
     @Override
     public Drone registerDrone(Drone drone) {
        Drone duplicate  = droneRepository.findTopBySerialNoAndSoftDeleteFalse(drone.getSerialNo()).orElse(null);
-       if (duplicate!= null){
-           throw  new ItemAlreadyExistException("Drone exists by serial number");
+       if (duplicate!= null) {
+           throw new ItemAlreadyExistException("Drone exists by serial number");
        }
-
+       return  droneRepository.save(drone);
     }
-
+    @Transactional
     @Override
     public Delivery loadDrone(long droneId, long medicineId) {
-        return null;
+        Drone drone = droneRepository.findByIdAndSoftDeleteFalse(droneId).orElse(null);
+        if (drone == null){
+            throw new ItemNotFoundException("Drone not found");
+        }
+        //check if medicine with given id exists
+        Optional<Medicine> medicine = medicineRepository.findMedicineByIdAndSoftDeleteFalse(medicineId);
+        if (medicine.isEmpty()){
+            throw new ItemNotFoundException("medicine not found");
+        }
+
     }
 
     @Override
