279a3cdf00acce92fdab2771364070ab46be89b7
felixmaina <karani.maina2010@gmail.com>
2022-09-12 12:00:14 +0300

    create an exception handleing package

--- src/main/java/com/example/drone/service/DroneAuditServiceImpl.java
+++ src/main/java/com/example/drone/service/DroneAuditServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.drone.model.Drone;
 import com.example.drone.model.DroneAudit;
 import com.example.drone.repository.DroneAuditRepository;
 import com.example.drone.repository.DroneRepository;
+import com.example.drone.service.exceptions.ItemNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
---
+++ src/main/java/com/example/drone/service/exceptions/ItemNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.drone.service.exceptions;
+
+public class ItemNotFoundException extends Throwable {
+    public ItemNotFoundException(String drone_not_found) {
+    }
+}
