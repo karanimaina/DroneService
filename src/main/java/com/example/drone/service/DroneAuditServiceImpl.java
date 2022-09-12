206f592de3d99907208a647f9c1f0a19e19e1353
felixmaina <karani.maina2010@gmail.com>
2022-09-12 10:22:40 +0300

    add the executor autowire the executor

--- src/main/java/com/example/drone/service/DroneAuditServiceImpl.java
+++ src/main/java/com/example/drone/service/DroneAuditServiceImpl.java
@@ -5,22 +5,26 @@ import com.example.drone.model.DroneAudit;
 import com.example.drone.repository.DroneAuditRepository;
 import com.example.drone.repository.DroneRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Executor;
+
 @Service
 @RequiredArgsConstructor
 public class DroneAuditServiceImpl implements DroneAuditService {
     private final DroneAuditRepository droneAuditRepository;
     private final DroneCommunicationService droneCommunicationService;
     private final DroneRepository droneRepository;
-
-    @A
+    @Autowired
+    @Qualifier("auditExecutor")
+    private Executor executor;
     @Override
     public void checkDronePercentages() {
     }
-
     @Override
     public List<DroneAudit> getDroneAuditByTimeRange(long droneId, Date startDate, Date endDate) {
         return null;
