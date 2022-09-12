cc47f2fa6f6c5882e3c534fd9b1c1dad56b27cf3
felixmaina <karani.maina2010@gmail.com>
2022-09-12 14:23:36 +0300

    check if drone exist

--- src/main/java/com/example/drone/service/DroneServiceImpl.java
+++ src/main/java/com/example/drone/service/DroneServiceImpl.java
@@ -94,19 +94,19 @@ public class DroneServiceImpl implements  DroneService{
         delivery.setLoadWeight (delivery.getLoadWeight ()+ medicine.getWeight ());
         return deliveryRepository.save (delivery);
     }
-//    /**
-//     *
-//     * @param droneId drone id to for loaded medicines
-//     * @return A list of loaded medicines int the drone
-//     */
-//    @Override
-//    public List<DeliveryLoad> checkLoadedMedication(long droneId) {
-//        //check if drone exists
-//        Drone drone= droneRepository.findByIdAndSoftDeleteFalse (droneId).orElse (null);
-//        if(drone== null){
-//            throw new ItemNotFoundException ("Drone does not exist");
-//        }
-//        // check if drone is in load state
+    /**
+     *
+     * @param droneId drone id to for loaded medicines
+     * @return A list of loaded medicines int the drone
+     */
+    @Override
+    public List<DeliveryLoad> checkLoadedMedication(long droneId) {
+        //check if drone exists
+        Drone drone= droneRepository.findByIdAndSoftDeleteFalse (droneId).orElse (null);
+        if(drone== null){
+            throw new ItemNotFoundException ("Drone does not exist");
+        }
+        // check if drone is in load state
 //        if(!(drone.getState ()==DroneState.LOADING || drone.getState ()==DroneState.DELIVERING))
 //            throw new IllegalStateException ("Drone is not in loading or delivering state");
 //        return deliveryLoadRepository.checkLoadedMedicationOnDrone (droneId);
