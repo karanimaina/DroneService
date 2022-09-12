39c56942436e16ae158576271e046dde5da13898
felixmaina <karani.maina2010@gmail.com>
2022-09-12 08:07:48 +0300

    add drone repository class

---
+++ src/main/java/com/example/drone/repository/DroneRepository.java
@@ -0,0 +1,9 @@
+package com.example.drone.repository;
+
+import com.example.drone.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DroneRepository  extends JpaRepository<Drone,Long> {
+    List
+
+}
